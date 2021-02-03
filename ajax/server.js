7574fd0ed63121894b40eeb96c6aab06b0c9a0b1
Breno Washington Lage de Araújo <42680340+Brenowla@users.noreply.github.com>
2021-02-03 20:32:30 -0300

    Ajax - parte 1

---
+++ ajax/server.js
@@ -0,0 +1,33 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+
+const app = express()
+
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback){
+        callback(null,'./upload')
+    },
+    filename: function (req, file, callback){
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({storage}).single('arquivo')
+
+app.post('/upload',(req,res) => {
+    upload(req,res,err => {
+        if(err) {
+            return res.end('Ocorreu um erro!')
+        }
+
+        res.end('Concluído com sucesso.')
+    })
+})
+
+app.listen(3030, () => console.log('Executando'))
